68e93d1af46ff09a80e7116f747874a43943ce8f
AdriFirst <adrien.nolhier@gmail.com>
2021-03-28 22:53:11 +0200

    zebi

--- init.el
+++ init.el
@@ -51,8 +51,8 @@
        workspaces        ; tab emulation, persistence & separate workspaces
        ;;zen               ; distraction-free coding or writing
 
-       ;;:editor
-       ;;(evil +everywhere); come to the dark side, we have cookies
+       :editor
+       (evil +everywhere); come to the dark side, we have cookies
        ;;file-templates    ; auto-snippets for empty files
        ;;fold              ; (nigh) universal code folding
        ;;(format +onsave)  ; automated prettiness
